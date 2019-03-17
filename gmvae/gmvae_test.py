6cba827d20c9c4ba88293828ec29b3deaa32d11e
suvalaki <david.ceglia.diaz@gmail.com>
2019-03-17 17:40:03 +1100

    working

--- gmvae/gmvae_test.py
+++ gmvae/gmvae_test.py
@@ -39,8 +39,34 @@ x = x_train[0:10000]
 y = y_train[0:10000]
 
 
-# GMVAE 
+#test building a dataset
+# https://kratzert.github.io/2017/06/15/example-of-tensorflows-new-input-pipeline.html
+# https://www.tensorflow.org/guide/datasets
 
+import tensorflow as tf
+# Create batched dataset
+dataset_x = tf.data.Dataset.from_tensor_slices((x))
+dataset_x.batch(100).make_initializable_iterator()
+batched_dataset = dataset_x.batch(100)
+
+# create itterator object
+iterator = batched_dataset.make_one_shot_iterator()
+next_element = iterator.get_next()
+
+sess.run(tf.global_variables_initializer())
+print(sess.run(next_element).shape)
+
+with tf.Session() as sess:
+    
+    sess.run(tf.global_variables_initializer())
+    sess.run(tf.local_variables_initializer())
+
+    print(sess.run(batched_dataset.range()))
+
+    print("sess.run(data_list): ", sess.run(next_element).shape, "\n")
+
+
+# GMVAE 
 import gmvae
 from gmvae import GMVAE
 
@@ -59,8 +85,8 @@ model.build()
 model.fit(
     X_train=x_train, 
     subsample=1000,
-    epochs=25,
-    iterep=100, 
+    epochs=200,
+    iterep=200, 
     y_train=y_train, 
     X_test=x_test, 
     y_test=y_test,
